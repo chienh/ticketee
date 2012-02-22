ca0d5d74287ffb96607f9cb47e5ce7a709b02167
Chien Hsiao <chienh@gmail.com>
2012-02-22 13:51:10 -0200

    Made projects only visible to users with permission to see them

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -23,9 +23,18 @@ describe ProjectsController do
   end
 
   it "displays an error for a missing project" do
+    sign_in(:user, user)
     get :show, :id => "not-here"
     response.should redirect_to(projects_path)
     message = "The project you were looking for could not be found"
     flash[:alert].should eql(message)
   end
+
+  it "cannot access the show action" do
+    sign_in(:user, user)
+    get :show, :id => project.id
+    response.should redirect_to(projects_path)
+    flash[:alert].should eql("The project you were looking for could not be found")
+  end
+
 end
